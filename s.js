f1e2684ee1fdb9e1a6487963583f2adf2e0512de
Saamel Sonn <saamel.sonn@gmail.com>
2022-05-01 23:35:47 +0300

    töötab

--- s.js
+++ s.js
@@ -46,10 +46,73 @@ function mapLoad(){
     }
 
     map.on('click', onMapClick);
+
+    shadow_line_points = [];
+    shadow_polygon = L.polygon(shadow_line_points).addTo(map);
+}
+
+function showShadow(){
+    /*if(polygon){
+        polygon.remove();
+    }*/
+    shadow_polygon.remove();
+    shadow_line_points = [];
+
+    var deg = 0.6; //kui mitme kraadi võrra täpselt otsida
+    var prev_in_shadow = isShadow(-90,-180);//kas eelmisena vaadatatud punkt oli varjus
+    var in_shadow = isShadow(-90,-180);//kas vaadatav punkt on varjus
+
+    var lat = 0;
+    var lng = -180;
+    ///* //ebatõhus, aga konsept töötab
+    for (let lng = -180; lng < 180; lng+=deg) {
+        for (let lat = -90; lat < 90; lat++) {
+            prev_in_shadow = in_shadow;
+            in_shadow = isShadow(lat,lng);
+            if(in_shadow != prev_in_shadow){//astusime üle varjupiiri
+                shadow_line_points.push([lat,lng]);
+                continue;
+            }
+        }
+    }//*/
+    /* //et ei skänniks nii palju mõttetult punkte kaardil ja käiks rohkem mööda varjujoont TODO: pooleli
+    while(lng < 180){
+        console.log(lng);
+        
+        prev_in_shadow = in_shadow;
+        in_shadow = isShadow(lat,lng);
+        console.log(in_shadow);
+
+        if(in_shadow != prev_in_shadow){//astusime üle varjupiiri
+            shadow_line_points.push([lat,lng-(deg/2)]);
+            lat+=deg;
+            
+        }else{
+            lng+=deg;
+        }
+        //}
+    }*/
+    //L.clearShapes(map);
+    
+    shadow_polygon = L.polygon(shadow_line_points).addTo(map);
+    //polygon.redraw();
+    //console.log(shadow_line_points);
+    
+}
+
+/**
+ * 
+ * @param {float} lat 
+ * @param {float} long 
+ * @returns boolean
+ */
+function isShadow(lat, long){
+    //console.log(SunCalc.getPosition(date_e.valueAsDate, lat, long).altitude);
+    return SunCalc.getPosition(date_e.valueAsDate, lat, long).altitude < 0; //võib panna -8 kraadi et oleks täitsa pime
 }
 
 function handleInput(){
-    selected_date = date_e.valueAsDate;
+    var selected_date = date_e.valueAsDate;
     //console.log(selected_date)
 
     //kontroll kas koordinaadid on lubatu piires
@@ -70,21 +133,21 @@ function handleInput(){
         return;
     }
 
-    asukoht = L.latLng(lat_e.value, long_e.value);
-    popup = L.popup();
+    //näita kaardil kohta
+    var asukoht = L.latLng(lat_e.value, long_e.value);
+    var popup = L.popup();
     popup.setLatLng(asukoht)
         .setContent(asukoht.lat+", "+asukoht.lng)
         .openOn(map);
 
-    times = SunCalc.getTimes(selected_date, lat_e.value, long_e.value);
-    console.log(times);
+    //kasutan SunCalc'i sest see peaks üsna täpne olema
+    var times = SunCalc.getTimes(selected_date, lat_e.value, long_e.value);
+    //console.log(times);
     
-    t_day_start = new Date(selected_date.getTime()).setHours(0,0,0,0)
-    //.getTime();//päeva algus 00:00
-    t_day_end = new Date(selected_date.getTime()).setHours(24,0,0,0)
+    var t_day_start = new Date(selected_date.getTime()).setHours(0,0,0,0)
+    //.getTime(); //päeva algus 00:00
+    var t_day_end = new Date(selected_date.getTime()).setHours(24,0,0,0)
     //.getTime(); //päeva lõpp 00:00
-    //console.log(day_beginning)
-    //console.log(day_end)
     
     output_e.innerHTML = "";
     
@@ -101,18 +164,19 @@ function handleInput(){
     output_e.innerHTML += "astronoomiline koidik: " + time_str(times.nightEnd) + "<br>";
     output_e.innerHTML += "astronoomiline loojang: " + time_str(times.night) + "<br>";
     
-    //arvutan ööaja pikkuse
+    //arvutan ööaja pikkuse (astronoomilise koidiku ja loojangu järgi, võiks ka nt päiksetõusu ja -loojangu järgi)
     var t_night_start = times.night.getTime();
     var t_night_end = times.nightEnd.getTime();
 
+    let nightime_total_millisec;
     if(t_night_start < t_night_end){
         nightime_total_millisec = t_night_end - t_night_start;
     }else{
         nightime_total_millisec = (t_night_end - t_day_start) + (t_day_end - t_night_start);
     }
 
-    nightime_total_minutes = Math.floor(nightime_total_millisec/1000/60);
-    nightime_total_hours = Math.floor(nightime_total_minutes/60);
+    let nightime_total_minutes = Math.floor(nightime_total_millisec/1000/60);
+    let nightime_total_hours = Math.floor(nightime_total_minutes/60);
 
     output_e.innerHTML += "Ööaeg kokku: " + nightime_total_hours + "h " + nightime_total_minutes%60 + "m";
     //output_e.innerHTML = "Ööaeg: 00:00-" + time_str(times.nightEnd) + ", "
@@ -125,8 +189,8 @@ function handleInput(){
  * @returns {String} time
  */
 function time_str(date){
-    hour_zero = "";
-    minute_zero = "";
+    let hour_zero = "";
+    let minute_zero = "";
     if(date.getHours()<10){ //et oleks alati hh:mm mitte h:mm
         hour_zero = "0";
     }
